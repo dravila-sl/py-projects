61cae58ec1b93bf28679e901dde7c50959591898
dravila-sl <serg.avilac@gmail.com>
2021-03-20 20:25:11 +0100

    Added function and print to block

--- ex02_c1v1-c2v2/c1v1_c2v2.py
+++ ex02_c1v1-c2v2/c1v1_c2v2.py
@@ -1,16 +1,19 @@
 #Make a program that calculates initial and final concentrations and volumes for reactions
 
-def regla3(a):
+def regla3(w,z,y):
     x = w*z/y
     return x
 
 value1 = input("Enter value to calculate: ")
-    if value1 = V2:
-        c1 = input("Enter C1: ")
-        v1 = input("Enter V1: ")
-        c2 = input("Enter C2: ")
-        regla3()
-    else value1 = C2:
-        c1 = input("Enter C1: ")
-        v1 = input("Enter V1: ")
-        v2= input("Enter V2: ")
+if value1 == V2:
+    c1 = input("Enter C1: ")
+    v1 = input("Enter V1: ")
+    c2 = input("Enter C2: ")
+    v2 = regla3(c1,v1,c2)
+    print("V2 is: ",v2)
+elif value1 == C2:
+    c1 = input("Enter C1: ")
+    v1 = input("Enter V1: ")
+    v2 = input("Enter V2: ")
+    c2 = regla3(c1,v1,v2)
+    print("C2 is: ",c2)
