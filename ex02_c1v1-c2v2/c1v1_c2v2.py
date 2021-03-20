113e2e30318bee4f0ff2a7b87506035a4b284ab6
Sergio Avila <serg.avilac@gmail.com>
2021-03-20 19:20:19 +0100

    Added if-else block

--- ex02_c1v1-c2v2/c1v1_c2v2.py
+++ ex02_c1v1-c2v2/c1v1_c2v2.py
@@ -4,6 +4,12 @@ def regla3(a):
     x = w*z/y
     return x
 
-input("Enter value to calculate: ")
-
-input("Enter value to calculate: ")
+value1 = input("Enter value to calculate: ")
+    if value1 = V2:
+        input("Enter C1: ")
+        input("Enter V1: ")
+        input("Enter C2: ")
+    else value1 = C2:
+        input("Enter C1: ")
+        input("Enter V1: ")
+        input("Enter V2: ")
