bc676ab10f9fb4651389f3921f47d31f743d2789
Sergio Avila <serg.avilac@gmail.com>
2021-03-20 13:17:00 +0100

    Solving conflicts between files 2-deleted extra text

--- ex02_c1v1_c2v2/c1v1_c2v2.py
+++ ex02_c1v1_c2v2/c1v1_c2v2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Make a program that calculates initial and final concentrations and volumes for reactions
 
 def regla3(a):
@@ -6,9 +5,3 @@ def regla3(a):
     return x
 
 input(Enter value to )
-=======
-hours = float(input("Enter hours: "))
-rate = float(input("Enter rate: "))
-pay = hours * rate
-print("Pay:",pay)
->>>>>>> 5cac9adc97fdf0136113e8039764d011cc941be1
